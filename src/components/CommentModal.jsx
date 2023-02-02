5bb2fdcba50fde685ad2640bb5fcedc5c470f635
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-02 02:40:59 -0300

    Lista de comentários é exibida com scroll

--- src/components/CommentModal.jsx
+++ src/components/CommentModal.jsx
@@ -7,66 +7,86 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import PropTypes from 'prop-types';
 import { getComments } from '../services/comments.services';
+import { Avatar, Card, CardActions, CardContent, CardHeader, IconButton } from '@mui/material';
 
 const style = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
-  width: 400,
-  bgcolor: 'background.paper',
-  border: '2px solid #000',
-  boxShadow: 24,
-  p: 4,
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    width: 460,
+    bgcolor: 'background.paper',
+    border: '1px solid #000',
+    boxShadow: 24,
+    p: 4,
 };
 
-export default function CommentModal(props) {
-  const [open, setOpen] = useState(false);
-  const [ comments, setComments ] = useState([]);
-  const handleOpen = () => setOpen(true);
-  const handleClose = () => setOpen(false);
+export default function CommentModal (props) {
+    const [open, setOpen] = useState(false);
+    const [comments, setComments] = useState([]);
+    const handleOpen = () => setOpen(true);
+    const handleClose = () => setOpen(false);
 
-  const fetchComments = async () => {
-    setComments(await getComments(props.id));
-  };
+    const fetchComments = async () => {
+        setComments(await getComments(props.id));
+    };
 
-  useEffect(() => {
-    fetchComments();
-  }, [])
+    useEffect(() => {
+        fetchComments();
+    }, [])
 
 
-  return (
-    <div>
-      <Button onClick={handleOpen}>Exibir comentários 📝</Button>
-      <Modal
-        aria-labelledby="transition-modal-title"
-        aria-describedby="transition-modal-description"
-        open={open}
-        onClose={handleClose}
-        closeAfterTransition
-        BackdropComponent={Backdrop}
-        BackdropProps={{
-          timeout: 500,
-        }}
-      >
-        <Fade in={open}>
-          <Box sx={style}>
-            <Typography id="transition-modal-title" variant="h6" component="h2">
-              Comentários 🗣️
-            </Typography>
-            {
-                comments.map((comment) => (
-                    <Typography key={ comment.id } id="transition-modal-description" sx={{ mt: 2 }}>
-                    {comment.body}
-                  </Typography>
-                ))
-            }
-           
-          </Box>
-        </Fade>
-      </Modal>
-    </div>
-  );
+    return (
+        <div>
+            <Button onClick={handleOpen}>Exibir comentários 📝</Button>
+            <Modal
+                aria-labelledby="transition-modal-title"
+                aria-describedby="transition-modal-description"
+                open={open}
+                onClose={handleClose}
+                closeAfterTransition
+                BackdropComponent={Backdrop}
+                BackdropProps={{
+                    timeout: 500,
+                }}
+            >
+                <Fade in={open}>
+                    <Box sx={style}>
+                        <Typography id="transition-modal-title" variant="h6" component="h2">
+                            Comentários 🗣️
+                        </Typography>
+                        <Box sx={{ maxHeight: '350px', overflowY: 'scroll' }}>
+                            {
+                                comments.map((comment) => (
+                                    <Card key={comment.id} component="div" sx={{ maxWidth: 460, marginTop: '.5rem' }}>
+                                        <CardHeader
+
+                                            avatar={
+                                                <Avatar sx={{ bgcolor: 'red' }} aria-label="user">
+                                                    {comment.name[0].toUpperCase()}
+                                                </Avatar>
+                                            }
+                                            title={comment.name}
+                                            subheader={comment.email}
+                                        />
+                                        <CardContent>
+                                            <Typography variant="body2" color="text.secondary">
+                                                {comment.body}
+                                            </Typography>
+                                        </CardContent>
+
+                                    </Card>
+                                ))
+                            }
+                        </Box>
+                        <Button onClick={handleClose}>
+                            Fechar
+                        </Button>
+                    </Box>
+                </Fade>
+            </Modal>
+        </div>
+    );
 }
 
 CommentModal.propTypes = {
