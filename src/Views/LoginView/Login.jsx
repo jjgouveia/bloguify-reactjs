2d103faad76bbac18795aaae94801824d2695790
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-02-03 21:17:06 -0300

    Lógica de login e de acessar sem precisar logar novamente caso tenha marcado o lembrar-me

--- src/Views/LoginView/Login.jsx
+++ src/Views/LoginView/Login.jsx
@@ -1,4 +1,4 @@
-import * as React from 'react';
+import React, { useEffect } from 'react';
 import Avatar from '@mui/material/Avatar';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
@@ -14,7 +14,7 @@ import { createTheme, ThemeProvider } from '@mui/material/styles';
 import { useForm } from "react-hook-form";
 import { useNavigate } from 'react-router-dom';
 
-function Copyright(props) {
+function Copyright (props) {
   return (
     <Typography variant="body2" color="text.secondary" align="center" {...props}>
       {'Copyright © '}
@@ -30,13 +30,29 @@ function Copyright(props) {
 const theme = createTheme();
 
 export default function LoginView () {
-  
+
   const redirect = useNavigate();
   const { register, handleSubmit, formState: { errors } } = useForm();
   const onSubmit = (data) => {
     localStorage.setItem('lorem', JSON.stringify(data));
     return redirect('/feed');
   };
+  const remember = JSON.parse(localStorage.getItem('lorem')) || [];
+  console.log(remember);
+
+  const isLogged = () => {
+    if(!remember) {
+      return;
+    };
+
+    if(remember.remember === true) {
+      redirect('/feed');
+    };    
+  };
+
+  useEffect(() => {
+    isLogged();
+  }, [remember.remember]);
 
 
   return (
@@ -68,42 +84,60 @@ export default function LoginView () {
             }}
           >
             <Avatar sx={{ m: 1, bgcolor: '#fff', width: '62px', height: '62px' }}>
-            <lord-icon
-    src="https://cdn.lordicon.com/xtrjgsiz.json"
-    trigger="hover"
-    colors="primary:#3a3347,secondary:#646e78,tertiary:#66a1ee"
-    style={{width:'56px', height:'56px'}}>
-</lord-icon>
+              <lord-icon
+                src="https://cdn.lordicon.com/xtrjgsiz.json"
+                trigger="hover"
+                colors="primary:#3a3347,secondary:#646e78,tertiary:#66a1ee"
+                style={{ width: '56px', height: '56px' }}>
+              </lord-icon>
             </Avatar>
             <Typography component="h1" variant="h5">
-            Entrar no Lorem Blog
+              Entrar no Lorem Blog
             </Typography>
             <Box component="form" noValidate onSubmit={handleSubmit(onSubmit)} sx={{ mt: 1 }}>
               <TextField
+                error={Boolean(errors?.email)}
                 margin="normal"
-                required
                 fullWidth
-                id="email"
                 label="Email"
                 autoComplete="email"
                 autoFocus
-                {...register("email")}
+                {...register("email", {
+                  required: {
+                    value: true,
+                    message: 'O email é obrigatório'
+                  },
+                  pattern: {
+                    value: /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i,
+                    message: 'Email inválido',
+                  },
+                })}
               />
+              <Box sx={{ fontSize: '12px' }}>
+                {errors.email && errors.email.message }
+              </Box>
               <TextField
+                error={Boolean(errors?.password)}
                 margin="normal"
-                required
                 fullWidth
                 label="Senha"
                 type="password"
                 id="password"
                 autoComplete="current-password"
-                {...register("password")}
-                
+                {...register("password", {
+                  required: true,
+                  minLength: 8,
+                })}
               />
+              <Box sx={{ fontSize: '12px' }}>
+                {errors.password?.type === 'required' && (<p>A senha é obrigatória</p>) }
+                {errors.password?.type === 'minLength' && (<p>A senha deve ter 8 caracteres</p>) }
+              </Box>
               <FormControlLabel
-                control={<Checkbox value="remember" color="primary" />}
-                label="Remember me"
+                control={<Checkbox color="primary" {...register("remember")} />}
+                label="Lembrar-me?"
               />
+
               <Button
                 type="submit"
                 fullWidth
@@ -115,12 +149,12 @@ export default function LoginView () {
               <Grid container>
                 <Grid item xs>
                   <Link href="#" variant="body2">
-                    Forgot password?
+                    Esqueceu a senha?
                   </Link>
                 </Grid>
                 <Grid item>
                   <Link href="#" variant="body2">
-                    {"Don't have an account? Sign Up"}
+                    {"Não tem uma conta? Vem cá!"}
                   </Link>
                 </Grid>
               </Grid>
