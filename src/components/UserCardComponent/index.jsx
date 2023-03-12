1ee2c4870a46d3646b148659454e4315e8583332
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-11 23:17:24 -0300

    refactor: Reorganização dos estilos do componente UserCardComponent

--- src/components/UserCardComponent/index.jsx
+++ src/components/UserCardComponent/index.jsx
@@ -2,13 +2,14 @@ import { Avatar, Card, CardActions, CardHeader} from '@mui/material'
 import React from 'react';
 import AppModal from '../AppModal';
 import PropTypes from 'prop-types';
+import style from './styles';
 
 export default function UserCardComponent ({ user }) {
   return (
-    <Card component="div" sx={ styles.cardContainer }>
+    <Card component="div" sx={ style.cardContainer }>
       <CardHeader
         avatar={
-          <Avatar sx={ styles.avatar } aria-label="user">
+          <Avatar sx={ style.avatar } aria-label="user">
             {user.name[0].toUpperCase()}
           </Avatar>
         }
@@ -22,21 +23,7 @@ export default function UserCardComponent ({ user }) {
   )
 };
 
-const styles = ({
-  cardContainer: {
-    display: 'flex',
-    justifyContent: 'space-between',
-    maxWidth: 500,
-    margin: '0 auto',
-    padding: '0rem 1rem',
-    marginTop: '1rem',
-    textAlign: 'justify',
-  },
 
-  avatar: {
-    bgcolor: 'red',
-    marginLeft: '-1rem' }
-});
 
 UserCardComponent.propTypes = {
   user: PropTypes.shape(),
---
+++ src/components/UserCardComponent/styles.js
@@ -0,0 +1,18 @@
+const style = {
+  cardContainer: {
+    display: 'flex',
+    justifyContent: 'space-between',
+    maxWidth: 500,
+    margin: '0 auto',
+    padding: '0rem 1rem',
+    marginTop: '1rem',
+    textAlign: 'justify',
+  },
+
+  avatar: {
+    bgcolor: 'red',
+    marginLeft: '-1rem'
+  }
+};
+
+export default style;
