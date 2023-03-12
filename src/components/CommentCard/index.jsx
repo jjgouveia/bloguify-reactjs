9b4f8313a8cc45c434e70a58f776ef6fbfcc0dd5
Jr Gouveia <gouvik.dev@gmail.com.br>
2023-03-11 22:54:12 -0300

    refactor: Refatoração na organização do comment card

--- src/components/CommentCard/index.jsx
+++ src/components/CommentCard/index.jsx
@@ -1,28 +1,29 @@
 import { Avatar, Card, CardContent, CardHeader, Typography } from '@mui/material';
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './style';
 
-export default function CommentCard({ comment }) {
- return (
-    <Card key={comment.id} component="div"
-    sx={{ maxWidth: 460, marginTop: '.5rem', bgcolor: 'background.paper', border: "1px solid #dadada" }}>
-        <CardHeader
-        sx={{ bgcolor: '#fafafa' }}
-            avatar={
-                <Avatar sx={{ bgcolor: '#7070F4' }} aria-label="user">
-                    {comment.name[0].toUpperCase()}
-                </Avatar>
-            }
-            title={comment.name}
-            subheader={comment.email}
-        />
-        <CardContent>
-            <Typography variant="body2" color="text.secondary">
-                {comment.body}
-            </Typography>
-        </CardContent>
-    </Card>
- );
+export default function CommentCard ({ comment }) {
+    return (
+        <Card key={comment.id} component="div"
+            sx={styles.container}>
+            <CardHeader
+                sx={styles.cardHeaderColor}
+                avatar={
+                    <Avatar sx={styles.avatarColor} aria-label="user">
+                        {comment.name[0].toUpperCase()}
+                    </Avatar>
+                }
+                title={comment.name}
+                subheader={comment.email}
+            />
+            <CardContent>
+                <Typography variant="body2" color="text.secondary">
+                    {comment.body}
+                </Typography>
+            </CardContent>
+        </Card>
+    );
 };
 
 CommentCard.propTypes = {
---
+++ src/components/CommentCard/style.js
@@ -0,0 +1,18 @@
+const styles = {
+  container: {
+    maxWidth: 460,
+    marginTop: '.5rem',
+    bgcolor: 'background.paper',
+    border: "1px solid #dadada"
+  },
+
+  cardHeaderColor: {
+    bgcolor: '#fafafa',
+  },
+
+  avatarColor: {
+    bgcolor: '#7070F4',
+  },
+};
+
+export default styles;
